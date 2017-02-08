d10f17c21a1d93b05f765bdc8bce9946df83a49e
sieniawski <sieniawski7@wp.pl>
2017-02-08 20:20:25 +0100

    this is 8.5 project

---
+++ js/8.5script.js
@@ -0,0 +1,12 @@
+var a = -2,
+	b = 3,
+	value = (a*a) + (2 * a * b) - (b*b);
+	
+
+if (value > 0) {	
+	console.log('Wynik dodatni.')	
+} else if (value < 0) {	
+	console.log('Wynik ujemny.')	
+} else {	
+	console.log('Wynik jest równy zero.')	
+}
